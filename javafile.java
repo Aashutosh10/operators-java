ff115d8605dd6c7797f37cb10d06b928a9ef5d2f
Aashutosh10 <aashutoshneupane12@gmail.com>
2024-09-13 14:31:46 +0545

    Initial commit

---
+++ javafile.java
@@ -0,0 +1,74 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args){
+        //length
+        //String name="Aashutosh";
+        //System.out.println(name.length());
+        //Array
+        // int[] marks={97,98,99};
+        // marks[0]=96;
+        // marks[1]=95;
+        // marks[2]=98;
+        // System.out.println(marks[0]);
+        //length
+        //System.err.println(marks.length);
+        //sort
+        // Arrays.sort(marks);
+        //2d array
+        //int[][] finalmarks = {{97, 98, 95}, {95, 98, 86}};
+        //System.out.println(finalmarks[1][0]);
+
+
+        //Casting
+
+        //double price =100.00;
+        //double finalprice =price + 18;
+        //System.out.println(finalprice);
+
+        //constant
+        //final float pi=3.14F;
+
+        //operators
+        //int a =3;
+        /*int b =5;
+        int sum= a+b;
+        System.err.println(sum);
+
+        int d =5;
+        int c =6;
+        int diff= d-c;
+        System.out.println(diff); */
+
+      /*   int a = 3;
+        int b = 5;
+        int mul= a*b;
+        System.out.println(mul);*/
+
+       /*  int a = 15;
+        int b = 5;
+        int divide= a / b;
+        System.err.println(divide);*/
+       /*  int a =1;
+        int b =2;
+        int mod= a % b;
+        System.err.println(mod);*/
+        //unary operator
+       /*int numb = 1;
+       numb++;
+       System.out.println( ++numb );*/
+
+        //System.out.println(Math.max(5, 6));
+        //System.out.println((int) Math.random()*100);
+
+        // input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input your age:");
+        int age = sc.nextInt();
+        System.out.print(age);
+        sc.close();
+    }
+
+}
